07d683496e7ac6e85fdc79ddc5f9762374658b6c
Phill Van Hoven <phillip.vanhoven@gmail.com>
2017-01-26 20:48:38 -0700

    initial commit

---
+++ Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open Argu
+
+type CliArguments =
+    | Export
+    | Install
+    | Listener of host:string * port:int
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Export _ -> "specify if the database should be exported into scripts."
+            | Install _ -> "specify if a new database instance should be created."
+            | Listener _ -> "specify a listener (hostname : port)."
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let parser = ArgumentParser.Create<CliArguments>(programName = "databaseinstallerfsharp.exe")
+    let result = parser.Parse(argv)
+    let all = result.GetAllResults()
+
+    let export = result.Contains<@ Export @>
+    if export then Console.WriteLine("Exporting")
+
+    if result.Contains<@ Install @> then Console.WriteLine("Installing")
+
+    if result.Contains<@ Listener @> then Console.WriteLine("Listener defined")
+    let listener = result.GetResult<@ Listener @>
+    if result.Contains<@ Listener @> then Console.WriteLine(listener);
+
+    Console.ReadLine() |> ignore
+
+    0 // return an integer exit code
